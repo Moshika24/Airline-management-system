cd62403798e9a91eda0229f941a978d90a450db8
mayankjaiswal27 <mayankjaiswal0104@gmail.com>
2025-03-21 21:05:16 +0530

    Created all directories with code

---
+++ airline-management-system/src/main/java/com/airline/dto/TicketRequest.java
@@ -0,0 +1,14 @@
+// src/main/java/com/airline/dto/TicketRequest.java
+package com.airline.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+public class TicketRequest {
+    @NotBlank(message = "Passenger name is required")
+    private String passengerName;
+    @NotNull(message = "Schedule ID is required")
+    private Long scheduleId;
+}
